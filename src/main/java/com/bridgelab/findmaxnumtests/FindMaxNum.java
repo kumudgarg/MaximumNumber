c31bf11cb55d23e5eeac121ecc549d52e54b263d
kumudgarg <kumud50garg@gmail.com>
2019-11-28 18:05:24 +0530

    Refactor 1 : all three to one generic method and find the maximum

--- src/main/java/com/bridgelab/findmaxnumtests/FindMaxNum.java
+++ src/main/java/com/bridgelab/findmaxnumtests/FindMaxNum.java
@@ -1,30 +1,13 @@
 package com.bridgelab.findmaxnumtests;
 
 public class FindMaxNum {
-    public Integer maxInteger(Integer firstNum, Integer secondNum, Integer thirdNum) {
-        Integer max = firstNum;
-        if (secondNum.compareTo(max) > 0)
-            max = secondNum;
-        if (thirdNum.compareTo(max) > 0)
-            max = thirdNum;
-        return max;
+    public static  <T extends Comparable<T>> T maximumValue(T firstVar, T secondVar, T thirdVar) {
+        T maxValue = firstVar;
+        if (secondVar.compareTo(maxValue) > 0)
+            maxValue = secondVar;
+        if (thirdVar.compareTo(maxValue) > 0)
+            maxValue = thirdVar;
+        return maxValue;
     }
 
-    public Float maxFloat(Float firstNum, Float secondNum, Float thirdNum) {
-        Float max = firstNum;
-        if (secondNum.compareTo(max) > 0)
-            max = secondNum;
-        if (thirdNum.compareTo(max) > 0)
-            max = thirdNum;
-        return max;
-    }
-
-    public Character maxChars(Character firstChar, Character secondChar, Character thirdChar) {
-        Character max = firstChar;
-        if (secondChar.compareTo(max) > 0)
-            max = secondChar;
-        if (thirdChar.compareTo(max) > 0)
-            max = thirdChar;
-        return max;
-    }
 }
--- src/test/java/com/bridgelab/findmaxnumtests/MaximumNumberTests.java
+++ src/test/java/com/bridgelab/findmaxnumtests/MaximumNumberTests.java
@@ -6,35 +6,13 @@ import org.junit.Test;
 public class MaximumNumberTests {
 
     @Test
-    public void givenIntegers_ShouldGiveMaximumInteger() {
+    public void givenSpecificDataTypeValue_ShouldReturnMaximumValue() {
         FindMaxNum findMaxNum = new FindMaxNum();
-        Integer maxNum = findMaxNum.maxInteger(23, 56, 11);
-        Assert.assertEquals((Integer) 56, maxNum);
-        Integer maxNum1 = findMaxNum.maxInteger(56, 23, 11);
-        Assert.assertEquals((Integer) 56, maxNum1);
-        Integer maxNum2 = findMaxNum.maxInteger(23, 11, 56);
-        Assert.assertEquals((Integer) 56, maxNum2);
-    }
-
-    @Test
-    public void givenFloats_ShouldGiveMaximumFloat() {
-        FindMaxNum findMaxNum = new FindMaxNum();
-        Float maxNum = findMaxNum.maxFloat(12.66f, 56.26f, 11.88f);
-        Assert.assertEquals((Float) 56.26f, maxNum);
-        Float maxNum1 = findMaxNum.maxFloat(56.26f, 12.66f, 11.88f);
-        Assert.assertEquals((Float) 56.26f, maxNum1);
-        Float maxNum2 = findMaxNum.maxFloat(12.66f, 11.88f, 56.26f);
-        Assert.assertEquals((Float) 56.26f, maxNum2);
-    }
-
-    @Test
-    public void givenChars_ShouldGiveMaximumChar() {
-        FindMaxNum findMaxNum = new FindMaxNum();
-        Character maxChar = findMaxNum.maxChars('w', 'p', 's');
-        Assert.assertEquals((Character) 'w', maxChar);
-        Character maxChar2 = findMaxNum.maxChars('p', 'w', 's');
-        Assert.assertEquals((Character) 'w', maxChar2);
-        Character maxChar3 = findMaxNum.maxChars('s', 'p', 'w');
-        Assert.assertEquals((Character) 'w', maxChar3);
+        Integer maxInt = findMaxNum.maximumValue(23, 56, 11);
+        Assert.assertEquals( 56, maxInt.intValue());
+        Float maxFloat = findMaxNum.maximumValue(12.66f, 56.26f, 11.88f);
+        Assert.assertEquals( (Float) 56.26f, maxFloat);
+        Character maxChar = findMaxNum.maximumValue('w', 'p', 's');
+        Assert.assertEquals('w', maxChar.charValue());
     }
 }
